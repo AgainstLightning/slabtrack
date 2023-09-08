794e80a407a653d1d0033564cdc4d6070eba0166
againstlightning <againstlightning@gmali.com>
2023-09-07 20:15:14 -0400

    add baked in test cert id

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 "use client"
+import { useCallback, useState } from 'react';
+import { useCgcData } from './useCgcData';
 
 export default function Home() {
-  const [certNumber, setCertNumber] = useState('');
-  const {data, isLoading} = useCgcData(certNumber)
+  const [certNumber, setCertNumber] = useState('4287278003');
+  const { data, isLoading } = useCgcData(certNumber);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
   }
+
+  const handleCertNumberChange = useCallback((e) => {
+    setCertNumber(e.target.value);
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -14,11 +20,11 @@ export default function Home() {
       <form onSubmit={handleSubmit}>
         <label>
           Certification Number:
-          <input type="text" value={certNumber} onChange={(e) => setCertNumber(e.target.value)} />
+          <input type="text" value={certNumber} onChange={handleCertNumberChange} />
         </label>
         <button type="submit">Submit</button>
       </form>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{isLoading ? "loading" : JSON.stringify(data, null, 2)}</pre>
     </main>
   );
 }
